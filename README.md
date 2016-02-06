edcdfbc866c817ea736ac7539ca42de6c039e5e9
Zane Hitchcox <zwhitchcox@gmail.com>
2016-02-05 23:38:21 -0500

    updated docs and imported React from babel in index.js

---
+++ README.md
@@ -0,0 +1,151 @@
+# html-jsx-loader
+
+Webpack loader module that exports HTML as [React](http://facebook.github.io/react/) JSX class.
+
+## Usage
+
+[Documentation: Using loaders](http://webpack.github.io/docs/using-loaders.html)
+
+## Examples
+
+Add the **html-jsx-loader** to your Webpack configuration:
+
+``` javascript
+{
+	...
+	module: {
+		loaders: [
+			{ test: /\.htm$/, loader: 'babel?presets[]=react,presets[]=es2015!html-jsx-loader'}
+		]},
+		resolve: {
+			extensions: ['', '.js', '.htm']
+		}
+	}
+	...
+}
+```
+
+Now you can write raw HTML (e.g. Introduction.htm) and webpack will automatically convert the content of this file to a React component.
+
+Finally, you can reference this component in your JavaScript code as follows:
+
+``` javascript
+	var Introduction = require('./Introduction'); 
+
+	//Introduction is not HTML but ReactJS class.
+```
+
+So, if you have an Introduction.html file like this:
+
+```html
+<h1>Hello</h1>
+<p>This is a greeting</p>
+```
+
+It is equivalent to saying
+
+```javascript
+var Introduction = React.createClass({
+	render() {
+		return (
+			<div>
+				<h1>Hello</h1>
+				<p>This is a greeting</p>
+			</div>
+		)
+	}
+})
+
+```
+
+
+## Advanced Usage
+
+### Grouping tags
+
+If you would like the **html-jsx-loader** to group tags into separate react components, use the query **group=true**:
+
+``` javascript
+{
+	...
+	module: {
+		loaders: [
+			{ test: /\.htm$/, loader: 'jsx-loader!imports?React=react!html-jsx-loader?group=true'}
+		]},
+		resolve: {
+			extensions: ['', '.js', '.htm']
+		}
+	}
+	...
+}
+```
+
+Then you can refer to your components like this:
+
+``` javascript
+	var Introduction = require('./Introduction');
+	var Header = Introduction.Header;
+	var Section = Introduction.Section;
+
+	//Section will contain all <section /> tags
+```
+
+### React Router integration
+
+If you would like the **html-jsx-loader** to parse **a** tags to React Router **Link** tags, specify data-* inside your markup and the conversion will be done automatically:
+
+
+``` html
+<html>
+	<body>
+		<a data-style="[color: 'white']" data-activeStyle="[color: 'red']" data-to="user" data-params="[userId: user.id]" data-query="[foo: bar]">[user.name]</a>
+	</body>
+</html>
+```
+
+``` javascript
+{
+	...
+	module: {
+		loaders: [
+			{ test: /\.htm$/, loader: 'jsx-loader!imports?React=react,Router=react-router,Link=>Router.Link!html-jsx-loader'}
+		]},
+		resolve: {
+			extensions: ['', '.js', '.htm']
+		}
+	}
+	...
+}
+```
+
+The only required attribute is **data-to**. If not provided, the current **a** tag will remain unchanged.
+
+### Variable Replacement
+
+If you would like the **html-jsx-loader** to replace a custom variable inside your HTML, create a variable in the format of \_\_VARIABLE_NAME\_\_ and add the desired replacement value as a query attribute to the loader. For Example:
+
+``` html
+<html>
+  <body>
+    <img src"__LOCATION__/image.png" />
+  </body>
+</html>
+```
+
+``` javascript
+{
+  ...
+  module: {
+    loaders: [
+      { test: /\.htm$/, loader: 'jsx-loader!imports?React=react!html-jsx-loader?__LOCATION__=server'}
+    ]},
+    resolve: {
+      extensions: ['', '.js', '.htm']
+    }
+  }
+  ...
+}
+```
+
+Then, the expected **src** path of the image tag will be: 'server/image.png'. If the query is not specified, the corresponding variable in the HTML will not change.
+
---
+++ index.js
@@ -0,0 +1,274 @@
+/**
+* © Copyright [2015] Hewlett-Packard Development Company, L.P.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+var HTMLtoJSX = require('htmltojsx');
+var loaderUtils = require('loader-utils');
+var jsdom = require('jsdom-no-contextify').jsdom;
+var defaultView = jsdom().defaultView;
+
+String.prototype.capitalize = function() {
+  return this.charAt(0).toUpperCase() + this.slice(1).toLowerCase();
+};
+
+function createElement(tag) {
+  return defaultView.document.createElement(tag);
+}
+
+function isEmpty(string) {
+   return !/[^\s]/.test(string);
+}
+
+var ATTRIBUTE_MAPPING = {
+  'classname': 'className',
+  'activestyle': 'activeStyle',
+  'htmlfor': 'htmlFor'
+};
+
+var TagToReactRouter = function() {
+
+  return {
+    reset: function() {
+      this.output = '';
+
+      this.routerLink = false;
+    },
+
+    oneLevelOnly: function(element) {
+
+      //based on https://github.com/reactjs/react-magic/blob/master/src/htmltojsx.js#L253
+      // Only a single child element
+      if (
+        element.childNodes.length === 1
+        && element.childNodes[0].nodeType === 1
+      ) {
+        return true;
+      }
+      // Only one element, and all other children are whitespace
+      var foundElement = false;
+      for (var i = 0, count = element.childNodes.length; i < count; i++) {
+        var child = element.childNodes[i];
+        if (child.nodeType === 1) {
+          if (foundElement) {
+            // Encountered an element after already encountering another one
+            // Therefore, more than one element at root level
+            return false;
+          } else {
+            foundElement = true;
+          }
+        } else if (child.nodeType === 3 && !isEmpty(child.textContent)) {
+          // Contains text content
+          return false;
+        }
+      }
+      return true;
+    },
+
+    parse: function(element) {
+
+      this.reset();
+
+      var wrapper = createElement('div');
+      wrapper.innerHTML = element;
+
+      if (this.oneLevelOnly(wrapper)) {
+        this.traverse(wrapper);
+      } else {
+        this.visit(wrapper);
+      }
+
+      return this.output;
+    },
+
+    traverse: function(element) {
+      for (var i = 0, count = element.childNodes.length; i < count; i++) {
+        this.visit(element.childNodes[i]);
+      }
+    },
+
+    visit: function(element) {
+      this.begin(element);
+      this.traverse(element);
+      this.end(element);
+    },
+
+    handleText: function(element) {
+      var text = element.textContent;
+      if (this.routerLink) {
+        text = text.replace(/\[/g, '{').replace(/]/g, '}');
+      }
+
+      var tempEl = createElement('div');
+      tempEl.textContent = text;
+
+      this.output += tempEl.innerHTML;
+    },
+
+    beginNode: function(node) {
+
+      var tagName = this.routerLink ? 'Link' : node.tagName.toLowerCase();
+      var attributes = [];
+      for (var i = 0, count = node.attributes.length; i < count; i++) {
+        var value = node.attributes[i].value;
+        if (value.indexOf('[') === -1) {
+          value = '"' + value + '"';
+        } else {
+          value = '{' + value.replace(/\[/g, '{').replace(/\]/g, '}') + '}';
+        }
+
+        var name = node.attributes[i].name;
+        if (this.routerLink) {
+          name = name.replace(/data-/g, '');
+        }
+
+        for (var key in ATTRIBUTE_MAPPING) {
+          if (ATTRIBUTE_MAPPING.hasOwnProperty(key)) {
+            name = name.replace(key, ATTRIBUTE_MAPPING[key]);
+          }
+        }
+        attributes.push(name + '=' + value);
+      }
+
+      this.output += '<' + tagName;
+      if (attributes.length > 0) {
+        this.output += ' ' + attributes.join(' ');
+      }
+      if (node.firstChild) {
+        this.output += '>';
+      }
+    },
+
+    begin: function(node) {
+      switch (node.nodeType) {
+        case 1:
+          if (node.tagName === 'A' && node.getAttribute('data-to')) {
+            this.routerLink = true;
+          }
+          this.beginNode(node);
+          break;
+
+        case 3:
+          this.handleText(node);
+          break;
+      }
+    },
+
+    end: function(node) {
+      if (node.nodeType === 1) {
+        var tagName = this.routerLink ? 'Link' : node.tagName.toLowerCase();
+        this.routerLink = false;
+        if (node.firstChild) {
+          this.output += '</' + tagName + '>';
+        } else {
+          this.output += ' />';
+        }
+      }
+    }
+  };
+};
+
+function createReactComponent(content) {
+  var converter = new HTMLtoJSX({
+    createClass: false
+  });
+
+  var indent = '  ';
+  var output = [
+    'React.createClass({\n',
+    indent,
+    'render: function() {\n',
+    indent + indent,
+    'return (\n',
+      converter.convert(new TagToReactRouter().parse(content)),
+    ');\n',
+    '}\n',
+    '})\n'
+  ].join('');
+
+  return output;
+}
+
+function getGroupedElements(content) {
+  var wrapperEl = createElement('div');
+  wrapperEl.innerHTML = content;
+  var elements = wrapperEl.getElementsByTagName('*');
+
+  var groupedElements = {};
+  for (var i = 0; i < elements.length;) {
+    var wrapper = createElement('div');
+    wrapper.appendChild(elements[i]);
+    var tag = elements[i].tagName;
+    if (groupedElements[tag]) {
+      groupedElements[tag].push(wrapper.innerHTML);
+    } else {
+      groupedElements[tag] = [wrapper.innerHTML];
+    }
+  }
+
+  return groupedElements;
+}
+
+function replaceVariables(content, query) {
+
+  var newContent = content;
+  for (var key in query) {
+    if (query.hasOwnProperty(key)) {
+      if (key.indexOf('__') === 0) {
+        var re = new RegExp(key, 'g');
+        newContent = newContent.replace(re, query[key]);
+      }
+    }
+  }
+
+  return newContent;
+
+}
+
+module.exports = function(content) {
+
+  var query = loaderUtils.parseQuery(this.query);
+
+  content = replaceVariables(content, query);
+
+  var output;
+
+  if (query.group) {
+
+    var groupedElements = getGroupedElements(content);
+
+    var tmp = '{';
+    var index = 0;
+    for (var key in groupedElements) {
+      if (groupedElements.hasOwnProperty(key)) {
+        var elements = groupedElements[key];
+
+        tmp += [key.capitalize(), ':', createReactComponent(elements.join(''))].join('');
+
+        if (index < Object.keys(groupedElements).length - 1) {
+          tmp += ',';
+        }
+
+        index++;
+      }
+
+    }
+    tmp += '};';
+
+    output = tmp;
+  } else {
+    output = createReactComponent(content) + ';';
+  }
+	return 'import React from \'react\'\nmodule.exports = ' + output;
+};
